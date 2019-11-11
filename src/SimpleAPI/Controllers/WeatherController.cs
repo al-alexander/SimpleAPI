3f5396f9fc578592357f59bf86bc344eb42c4bec
Alexander M <alalexanderm@gmail.com>
2019-11-11 10:17:02 +0100

    Modified WeattherController with rnd.

--- src/SimpleAPI/Controllers/WeatherController.cs
+++ src/SimpleAPI/Controllers/WeatherController.cs
@@ -21,6 +21,7 @@ namespace SimpleAPI.Controllers
         public WeatherController()
         {
             _logger = null;
+            var rng = new Random();
         }
         public WeatherController(ILogger<WeatherForecastController> logger)
         {
